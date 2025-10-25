c96682d7a3e8bc561f1b14c448aa6a5caaebda2f
Semini Herath <semiwasanaherath@gmail.com>
2025-10-25 22:19:03 +0530

    Auth page updated

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'main.dart'; // To access AuthView enum
-import 'constants.dart';
 
 // --- LOGIN SCREEN ---
 class LoginScreen extends StatefulWidget {
